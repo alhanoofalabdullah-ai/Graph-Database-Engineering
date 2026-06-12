a862cfe0a6feb48ffd98ad436b3ba480f6651e85
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-12 11:39:46 +0300

    Create relationship_analysis.py

---
+++ project-03-customer-relationship-graph/relationship_analysis.py
@@ -0,0 +1,13 @@
+relationships = {
+
+    "Customer Referrals": 25,
+
+    "Shared Interests": 120,
+
+    "Repeat Purchases": 340
+
+}
+
+for key, value in relationships.items():
+
+    print(key, value)
