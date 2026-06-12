b7ea5545560048efe1e04645df6222b569f76269
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-12 16:25:19 +0300

    Create recommendation_engine.py

---
+++ project-05-recommendation-engine-graph/recommendation_engine.py
@@ -0,0 +1,13 @@
+recommendations = {
+
+    "Ahmed": ["Tablet", "Smart Watch"],
+
+    "Sara": ["Laptop", "Headphones"]
+
+}
+
+for customer, products in recommendations.items():
+
+    print(customer)
+
+    print(products)
