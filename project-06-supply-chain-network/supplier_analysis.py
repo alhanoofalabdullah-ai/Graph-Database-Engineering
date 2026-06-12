ca8462053192abce24c7b6a1276f9ff257f1193a
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-12 16:39:18 +0300

    Create supplier_analysis.py

---
+++ project-06-supply-chain-network/supplier_analysis.py
@@ -0,0 +1,13 @@
+suppliers = {
+
+    "Global Electronics": 95,
+
+    "Tech Components": 87,
+
+    "Industrial Supply Co": 80
+
+}
+
+for supplier, score in suppliers.items():
+
+    print(supplier, score)
