b63ebd3dea555efa527a4585d3224652e098d1db
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-12 11:12:16 +0300

    Create neo4j_connection.py

---
+++ project-01-neo4j-fundamentals/neo4j_connection.py
@@ -0,0 +1,12 @@
+from neo4j import GraphDatabase
+
+uri = "bolt://localhost:7687"
+
+driver = GraphDatabase.driver(
+    uri,
+    auth=("neo4j", "password123")
+)
+
+print("Connected Successfully")
+
+driver.close()
