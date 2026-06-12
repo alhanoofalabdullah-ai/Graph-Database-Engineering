145b3391f5640a4d07702461bb9745f72f5f6d60
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-12 16:58:16 +0300

    Create knowledge_loader.py

---
+++ project-07-knowledge-graph-platform/knowledge_loader.py
@@ -0,0 +1,15 @@
+concepts = [
+
+    "Digital Transformation",
+
+    "Artificial Intelligence",
+
+    "Data Governance",
+
+    "Cybersecurity"
+
+]
+
+for concept in concepts:
+
+    print(concept)
