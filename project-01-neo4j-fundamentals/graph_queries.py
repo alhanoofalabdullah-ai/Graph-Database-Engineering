b7663366f6206271e7977d42c4b3ea0de322a562
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-12 11:14:34 +0300

    Create graph_queries.py

---
+++ project-01-neo4j-fundamentals/graph_queries.py
@@ -0,0 +1,21 @@
+from neo4j import GraphDatabase
+
+driver = GraphDatabase.driver(
+    "bolt://localhost:7687",
+    auth=("neo4j", "password123")
+)
+
+query = """
+MATCH (c:Customer)
+RETURN c.name
+"""
+
+with driver.session() as session:
+
+    result = session.run(query)
+
+    for record in result:
+
+        print(record)
+
+driver.close()
