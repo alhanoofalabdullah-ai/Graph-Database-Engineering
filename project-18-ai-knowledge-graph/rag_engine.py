d12060165b5eeaa15c16414b9a840dfe9a64c308
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-19 14:56:03 +0300

    Create rag_engine.py

---
+++ project-18-ai-knowledge-graph/rag_engine.py
@@ -0,0 +1,11 @@
+question = "What is Artificial Intelligence?"
+
+context = [
+    "Artificial Intelligence includes Machine Learning",
+    "Knowledge Graphs improve reasoning"
+]
+
+print(question)
+
+for item in context:
+    print(item)
